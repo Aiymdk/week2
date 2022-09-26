a01964ff53792c51680e269a0b3963ba34f22ab6
Aiymdk <97492001+Aiymdk@users.noreply.github.com>
2022-09-26 16:12:08 +0600

    Add files via upload

---
+++ week2 python practice work/2task4.py
@@ -0,0 +1,8 @@
+s= [90, 300, 50, 30, 150, 20] 
+Ln=s[0] 
+for number in s: 
+    if number>Ln: 
+        Ln=number 
+a=len(s) 
+print(Ln/a) 
+ 
---
+++ week2 python practice work/2task5.py
@@ -0,0 +1,3 @@
+s= [15, 25, 3, 58, 162, 1] 
+s.sort(reverse=True) 
+print(s)
